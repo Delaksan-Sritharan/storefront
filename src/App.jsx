55f1ba887eeb56e037014cea6669bdbfbbc345fb
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-28 17:39:20 +0530

    border color changed to black

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import User from "./Dela";
 
 function App() {
   return (
-    <div className="border-2 border-red-500">
+    <div className="border-2 border-black">
       <User name="Dela" age={24}/>
       <User name="sesa"/>
       
