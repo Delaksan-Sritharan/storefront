4e4abd8dec9eb9327fcd23645af79ea8be5a73c8
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-28 11:14:55 +0530

    storefront frontend

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Dela from "./Dela";
+import Button from "./Button";
+
+function App() {
+  return (
+    <>
+      <Dela name="Dela"/>
+      <Dela name="sesa"/>
+      <Button />
+      <Button />
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Dela.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dela(props) {
+  return <h1 className="heading">{props.name}</h1>;
+}
+
+export default Dela;
