dfd70f650aa444e78dec75b531a0e01eed936a0d
Delaksan-Sritharan <delaksan@gmail.com>
2025-09-18 09:44:42 +0530

    home page changed

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,9 +1,25 @@
-const HotelsPage = () => {
+import Hero from "../components/Hero";
+
+import HotelListings from "../components/HotelListing";
+import { Button } from "@/components/ui/button";
+import { getAllHotels } from "@/lib/api";
+
+function HomePage() {
   return (
-    <main className="px-4">
-      <h1 className="text-2xl font-bold">Hotels</h1>
+    <main>
+      <div className="relative min-h-[85vh]">
+        <Hero />
+      </div>
+      <Button
+        onClick={() => {
+           getAllHotels();
+        }}
+      >
+        GET HOTELS
+      </Button>
+      <HotelListings />
     </main>
   );
-};
+}
 
-export default HotelsPage;
+export default HomePage;
