826bd7ebdc3a8d462dee187698bc046a8c28e005
Delaksan-Sritharan <delaksan@gmail.com>
2025-10-04 13:12:30 +0530

    added try catch

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,35 +1,19 @@
 const getAllHotels = async () => {
-    // TODO: Make a GET request to the API\
-    const res = await fetch("http://localhost:8000/api/hotels", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    const data = await res.json();
-    console.log(data);
+    try {
+        const res = await fetch("http://localhost:8000/api/hotels", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await res.json();
+        console.log(data);
 
+        return data;
 
-    //   console.log("A");
-    //   fetch("http://localhost:8000/api/hotels", {
-    //     method: "GET",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //   })
-    //     .then((res) => {
-    //       console.log(res);
-    //       return res.json();
-    //     })
-    //     .then((data) => {
-    //       console.log(data);
-    //     })
-    //     .catch((error) => {
-    //       console.log(error);
-    //     });
-    //   console.log("B");
-    // TODO: Return the data
-    return data;
-};
+    } catch (error) {
+        throw new Error(error.message);
+    };
+}
 
-export { getAllHotels };
+export { getAllHotels }
